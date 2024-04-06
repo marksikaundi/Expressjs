2021778e9b6316b2ed34f6750833c797eeaa5da0
marksikaundi <emmanuelmark117@gmail.com>
2024-04-06 22:49:41 +0200

    Add route for root endpoint

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (request, response) => {
+    response.send("Hello World")
+})
+
 app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}`)
 })
