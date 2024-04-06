1ac3c62a1c3b9057359d8546d21acb7e7cbceccd
marksikaundi <emmanuelmark117@gmail.com>
2024-04-06 23:12:35 +0200

    Add API endpoint for retrieving users

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,21 @@
-import express from "express";
+import express, { response } from "express";
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
-    response.send("Hello World")
-})
+app.get("/", (request, response) => {
+  response.send("Hello World");
+});
+
+app.get("/api/users", (request, response) => {
+  response.send([{ id: 1, username: "mark", displayName: "Mark Sikaundi" },
+  { id: 2, username: "terry", displayName: "Tery M" },
+  { id: 3, username: "joe", displayName: "Joe Y" },
+  { id: 4, username: "sarah", displayName: "Sarah" }
+]);
+});
 
 app.listen(PORT, () => {
-    console.log(`Running on PORT ${PORT}`)
-})
-
-
-
+  console.log(`Running on PORT ${PORT}`);
+});
