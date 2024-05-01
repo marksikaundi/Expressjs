13802596ea91c5e2591c9bb3067ade357b20d6f9
marksikaundi <emmanuelmark117@gmail.com>
2024-05-01 20:08:44 +0200

    tested my first get method

--- src/index.mjs
+++ src/index.mjs
@@ -4,9 +4,28 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+// localhost:3000
+// localhost:3000/users
+// localhost:3000/users/1
+// localhost:3000/products
 
 
 
